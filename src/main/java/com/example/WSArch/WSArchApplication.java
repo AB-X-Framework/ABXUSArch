5572ac8d9a94f0e57f62770dce889ade1f7e735f
luis <llara@appvance.com>
2024-12-23 12:35:18 -0600

    test

---
+++ src/main/java/com/example/WSArch/WSArchApplication.java
@@ -0,0 +1,11 @@
+package org.abx.wsarch;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class WSArchApplication {
+    public static void main(String[] args) {
+        SpringApplication.run(WSArchApplication.class, args);
+    }
+}
